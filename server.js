4f263184afa38d1f1deaa96d9c333695d5d1c8c0
baixiaofeng <xiaofeng.bai@unicareer.com>
2019-12-02 18:56:34 +0800

    add log

--- server.js
+++ server.js
@@ -4,16 +4,29 @@ import router from "./src/router/index";
 import config from "./config/base";
 import db from "./config/mongo";
 import koaBody from "koa-body";
+import log4js from "koa-log4";
 // import koaBody from "koa-bodyparser";
 
 const app = new Koa();
+const logger = log4js.getLogger("app");
+
+app.use(log4js.koaLogger(log4js.getLogger("http"), { level: "auto" }));
+
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(2222, `${ctx.method} ${ctx.url} - ${ms}ms`);
+  logger.info(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
 db.initBase();
 app.use(
   koaBody({
     multipart: true,
     formidable: {
       maxFileSize: 200 * 1024 * 10240,
-      uploadDir: "upload/",
+      uploadDir: "upload/"
       // onFileBegin: (name, file) => {
       //   // const fileFormat = file.name.split(".");
       //   // file.name = `${Date.now()}.${fileFormat[fileFormat.length - 1]}`;
@@ -23,6 +36,11 @@ app.use(
   })
 );
 app.use(router.routes()).use(router.allowedMethods());
+
+app.on("error", function(err, ctx) {
+  console.error(11111, err);
+  logger.error("server error", err, ctx);
+});
 
 if (process.argv[2] && process.argv[2][0] == "c") {
   const repl = require("repl");
@@ -37,6 +55,6 @@ if (process.argv[2] && process.argv[2][0] == "c") {
     });
 } else {
   app.listen(config.port, config.ip, () => {
-    console.log("connect", `server ${config.ip + ":" + config.port} created`);
+    console.info("connect", `server ${config.ip + ":" + config.port} created`);
   });
 }
